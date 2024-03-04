6ab20bd97605cf2a6862aeba1d58e7e6460af659
oladsdan <104092480+oladsdan@users.noreply.github.com>
2024-03-04 11:48:42 +0100

    corrected the cors

--- api/config/allowedOrigins.js
+++ api/config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins =["http://localhost:3000", "sanairbnb.netlify.app"]
+const allowedOrigins =["http://localhost:3000", "https://sanairbnb.netlify.app"]
 
 export default allowedOrigins
